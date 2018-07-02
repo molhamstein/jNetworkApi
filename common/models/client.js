8d8381cb0a341f044da929593cf969e1accf0c77
amer alhosary <amer.alhosary1993@gmail.com>
2018-07-02 21:08:21 +0300

    set message

--- common/models/client.js
+++ common/models/client.js
@@ -24,7 +24,7 @@ module.exports = function(Client) {
 				  } 
 				});
 		  http.get(
-					'http://services.mtn.com.sy/General/MTNSERVICES/ConcatenatedSender.aspx?User=LEMA%20ISP%202013&Pass=L1E2M3A4&From=LEMA-ISP&Gsm='+(client.mobile).substr(2)+'&Msg=Your Verification Code '+String(code)+'&Lang=0&Flash=0',
+					'http://services.mtn.com.sy/General/MTNSERVICES/ConcatenatedSender.aspx?User=LEMA%20ISP%202013&Pass=L1E2M3A4&From=LEMA-ISP&Gsm='+(client.mobile).substr(2)+'&Msg=Your%20Verification%20Code '+String(code)+'&Lang=0&Flash=0',
 					  function(res) {
 						res.on('data', function(data) {
 							console.log(data.toString());
