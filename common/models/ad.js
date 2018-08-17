1de7a849e029758cec6dcacdc4fd921c935297b8
amer alhosary <amer.alhosary1993@gmail.com>
2018-08-18 00:00:59 +0300

    add link property and random ad

--- common/models/ad.js
+++ common/models/ad.js
@@ -1,5 +1,23 @@
 'use strict';
-
+var app = require('../../server/server');
+const connector = app.dataSources.mydb.connector;
 module.exports = function(Ad) {
-
+    Ad.randomAD = function(cb) {
+		var adM = app.models.AD;
+			 
+        //adM.findOne({where: { status: 1 },order: 'RAND()'}, cb);
+        var sql ="select * from AD where status=1 order by RAND() limit 1";
+        connector.execute(sql, null, (err, resultObjects) => {
+            if(!err){
+                    process.nextTick(function() {
+                       cb(null, resultObjects);
+                 });
+            }
+            else
+                 process.nextTick(function() {
+                   cb(err, null);
+                 });
+                //next();
+         });
+  }
 };
