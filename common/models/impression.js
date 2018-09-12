cc78c1f9eb1d9dd33de47dbb395215d5a1c83754
ahmad-ataya <ahmad.3taya@gmail.com>
2018-09-12 10:30:22 +0300

    get mobile insted of clientId for click&impresion

--- common/models/impression.js
+++ common/models/impression.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Impression) {
+	Impression.beforeRemote('create', function( ctx, modelInstance, next) {
+		if(!ctx.req.body.mobile)
+			return next();
+		
+		Impression.app.models.client.findOne({where : {mobile : ctx.req.body.mobile}},function(err,client){
+			if(err)
+				return next(err);
+			if(client)
+				ctx.req.body.client_id = client.id;
+			delete ctx.req.body.mobile;
+			return next();
+		});
+	});
 
 };
