35763d301fa413b1a1c17ee4fa645e907c2d9b61
Your Name <you@example.com>
2018-09-25 12:44:10 +0300

    T

--- common/models/client.js
+++ common/models/client.js
@@ -214,10 +214,13 @@ module.exports = function(Client) {
     Client.customSms = function(mobile,message,cb){
     	console.log(mobile,message);
 		http.get(
-			'http://services.mtn.com.sy/General/MTNSERVICES/ConcatenatedSender.aspx?User=LEMA%20ISP%202013&Pass=L1E2M3A4&From=LEMA-ISP&Gsm='+mobile+'&Msg='+message+'&Lang=0&Flash=0',
+			'http://services.mtn.com.sy/General/MTNSERVICES/ConcatenatedSender.aspx?User=LEMA%20ISP%202013&Pass=L1E2M3A4&From=LEMA-ISP&Gsm='+mobile.substring(2)+'&Msg='+String(message)+'&Lang=0&Flash=0',
 		function(res) {
 			res.on('data', function(data) {
-				console.log(data.toString());
+				console.log(data.toString())
+				if(data != mobile.substring(2))
+					return cb(new Error('mobileWrong'));
+                                   //console.log(data.toString());
 			  return cb(null,'Done');
 			});
 		  }
@@ -236,7 +239,7 @@ module.exports = function(Client) {
     	description: 'send message to user',
 		accepts: [
 			{arg: 'mobile', type: 'string', required: true, http: {source: 'form'}},
-			{arg: 'message', type: 'string', required: true, http: {source: 'format'}},
+			{arg: 'message', type: 'string', required: true, http: {source: 'form'}},
 		],
 		returns: {arg: 'message', type: 'string'},
 		http: {verb: 'post',path: '/customSms'},
