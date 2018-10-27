a1a8bbe031bab862dfcc8e1fc5f5ff0e7dfdbd0f
ahmad-ataya <ahmad.3taya@gmail.com>
2018-10-28 01:15:31 +0200

    T

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -3,4 +3,20 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+
+  server.models.partner.find({where : {email : 'a@a.com'}},function(err,user){
+  	// console.log(user)
+
+  	server.models.Role.find({where : {name : 'admin'}},function(err,admin){
+  		// console.log(admin)
+  		// admin[0].principals.create({
+	   //      principalType: 'partner',
+	   //      principalId: user[0].id
+	   //    }, function(err, principal) {
+	   //      if (err) throw err;
+
+	   //      console.log('Created principal:', principal);
+	   //    });
+  	});
+  })
 };
